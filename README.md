5f1e83032d9068eab8cc1f1ac8e7f7a7f43f7220
Rockey <rockeykumar1901@gmail.com>
2022-06-24 20:31:04 +0530

    initial commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# springBootFileUploadApplication
---
+++ src/test/java/com/rajtech/springbootfileupload/SpringBootFileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajtech.springbootfileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFileUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
